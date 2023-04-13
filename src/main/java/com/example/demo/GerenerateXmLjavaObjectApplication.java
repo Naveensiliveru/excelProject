c97e89bdeba4dfbdd5e81acce937b316b1090e97
naveen <naveensiliveru143@gmail.com>
2023-04-13 12:38:31 +0530

    add project

---
+++ src/main/java/com/example/demo/GerenerateXmLjavaObjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenerateXmLjavaObjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenerateXmLjavaObjectApplication.class, args);
+	}
+
+}
